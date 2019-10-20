e033ad7d0a3793b33532f1d110f81e86e6e5528a
user <user@users-MacBook-Pro.local>
2019-10-20 11:45:23 +0300

    Bubble sort implemented

---
+++ index.js
@@ -0,0 +1,21 @@
+function bubbleSort(arr) {
+  for (let i = arr.length; i > 0; i--) {
+    let max = 0;
+
+    for (let j = 1; j < i; j++) {
+      if (arr[max] > arr[j]) {
+        [arr[j], arr[max]] = [arr[max], arr[j]];
+      }
+
+      max++;
+    }
+  }
+
+  return arr;
+}
+
+const res = bubbleSort([2,7,1,5,3])
+
+console.log(res)
+
+console.log(bubbleSort([3,1,2,2,5,-1]))
